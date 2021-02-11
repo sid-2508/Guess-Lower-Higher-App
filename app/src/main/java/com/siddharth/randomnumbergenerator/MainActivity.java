0a7a2faa5568b8065a883cdea0408d26ce5981ce
Siddharth Manghwani <sid.m2508@gmail.com>
2021-02-12 01:14:39 +0530

    Complete Game

--- app/src/main/java/com/siddharth/randomnumbergenerator/MainActivity.java
+++ app/src/main/java/com/siddharth/randomnumbergenerator/MainActivity.java
@@ -4,22 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void generate(View view){
-        Random random = new Random();
-        int n = random.nextInt(20) + 1;
-        String i = String.valueOf(n);
-        Toast.makeText(this, i.toString() , Toast.LENGTH_SHORT).show();
+    int n;
+
+    public void guess(View view){
+        EditText value = (EditText) findViewById(R.id.input);
+        int i = Integer.parseInt(value.getText().toString());
+        if(i<n) {
+            Toast.makeText(this, "Higher", Toast.LENGTH_SHORT).show();
+        }
+        else if(i>n)
+        {
+            Toast.makeText(this, "Lower", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(this, "Correct", Toast.LENGTH_SHORT).show();
+            Random random = new Random();
+            n = random.nextInt(20) + 1;
+        }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Random random = new Random();
+         n = random.nextInt(20) + 1;
     }
 }
